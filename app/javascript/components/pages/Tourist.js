45e710b6e94617b302157fec2d18a80b29f77a3a
learn <learn@learn10.local>
2019-04-23 13:49:58 -0700

    CSS and Cards

--- app/javascript/components/pages/Tourist.js
+++ app/javascript/components/pages/Tourist.js
@@ -1,10 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import {Link} from 'react-router-dom'
+import MainImage from '../../images/SanDayGo-Header-Background.jpg';
+import GenericImage from '../../images/600Holder.jpg'
 import {
   Container,
   Row,
-  Col
+  Col,
+  Card,
+  Button
 } from 'react-bootstrap'
 
 class Tourist extends React.Component {
@@ -12,51 +16,163 @@ class Tourist extends React.Component {
     return (
       <React.Fragment>
         <div>
-          <section>
-            <h1>Header goes Here</h1>
-            <hr/>
-          </section>
+        <div className='tourist-image'>
+            <img src={MainImage} alt='San Diego Skyline' />
+        </div>
             <section>
-              <h1>Welcome to San Day-Go!</h1>
+              <h2>Welcome To San-DayGo!</h2>
+                <p>Located on the coast of the Pacific Ocean in Southern California, San Diego is widely known as “America’s Finest City.” Famous for its 70 miles and of white-sand beaches and amazing weather, the city offers an abundance of fun attractions for visitors of all ages.</p>
+                <p>Boasting a citywide population of more than 1.3 million and more than 3 million residents countywide, San Diego is California’s second largest city and the United States’ eighth largest. San Diego County is made up of 18 incorporate cities and towns along with other charismatic neighborhoods and communities, such as Carlsbad, Coronado, Del Mar, Downtown San Diego’s Gaslamp Quarter, Point Loma, Chula Vista, Old Town, La Jolla, and North Park.</p>
+                <p>Explore all the world-class attractions San Diego has to offer, including the world-famous Balboa Park, San Diego Zoo and San Diego Zoo Safari Park, SeaWorld San Diego and LEGOLAND California, as well as the abundance of extraordinary culinary experiences at Celebrity Chef-helmed restaurants located throughout the county.</p>
+                <p>Enjoy your Day-on-the-Go right here in San Diego!</p>
+            </section>
+            <hr/>
+            <section>
+              <h2>How it Works</h2>
                 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                </p>
+            </section>
+            <hr/>
+            <section>
+              <h2>Choose Your Adventure!</h2>
 
-                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-            </section>
-            <hr/>
-            <section>
-              <h1>How it Works</h1>
-                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                </p>
-            </section>
-            <hr/>
-            <section>
-              <h1>Choose Your Adventure!</h1>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '20rem', height: 'auto'  }}>
+                <Card.Img variant='top' src={GenericImage} />
+                <Card.Body>
+                  <br />
+                  <Card.Title>ADVENTUROUS</Card.Title>
+                  <Card.Text>
+                    Experience the adreniline rush with a day filled with high-energy experiences.
+                  </Card.Text>
+                  <Button variant="primary">Make This Itinery</Button>
+                  <br />
+                  <br />
+                </Card.Body>
+              </Card>
+
                   <Container>
                     <Row>
-                      <Col>1 of 5</Col>
                       <Col>Adventerous</Col>
                       <Col>Foodie</Col>
                       <Col>Beach Bum</Col>
-                      <Col>5 of 5</Col>
                     </Row>
                   </Container>
                   <Container>
                     <Row>
-                      <Col>1 of 5</Col>
-                      <Col>Family</Col>
+                    <Col>Family</Col>
                       <Col>Sporty</Col>
                       <Col>Cultural</Col>
-                      <Col>5 of 5</Col>
                     </Row>
                   </Container>
                   <Container>
                     <Row>
-                      <Col>1 of 5</Col>
                       <Col>Fashonista</Col>
                       <Col>LGBT</Col>
                       <Col>Outdoors</Col>
-                      <Col>5 of 5</Col>
                     </Row>
                   </Container>
             </section><hr/>
