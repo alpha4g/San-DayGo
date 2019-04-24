3ef3c4691d2cac13b50826e40527bf7df2e2afaa
learn <learn@learn16.localdomain>
2019-04-23 17:01:16 -0700

    View and Delete Work, Edit is work in Progress

--- app/javascript/components/pages/EditModal.js
+++ app/javascript/components/pages/EditModal.js
@@ -18,66 +18,93 @@ class EditModal extends Component {
                 phone_number:'',
                 hours:''
             },
-            modalEditShow:true
+            modalShow:false
         }
     }
 
+    experienceTypeHandler = (event)=> {
+        this.setState({experience_type: event.target.value});
+    }
+    experienceSubTypeHandler = (event) => {
+        this.setState({experience_sub_type: event.target.value});
+    }
+    experienceDescriptionHandler = (event) => {
+        this.setState({experience_description: event.target.value});
+    }
+    addressHandler = (event) => {
+        this.setState({address: event.target.value});
+    }
+    websiteHandler = (event) => {
+        this.setState({website: event.target.value});
+    }
+    phoneNumberHandler = (event) => {
+        this.setState({phone_number: event.target.value});
+    }
+    hoursHandler = (event) => {
+        this.setState({hours: event.target.value});
+    }
+    handleSave = (event) => {
+        event.preventDefault();
+        const form = {
+            experience_name:this.state.experience_name,
+            experience_type:this.state.experience_type,
+            experience_sub_type:this.state.xperience_sub_type,
+            experience_description:this.state.experience_description,
+            address:this.state.address,
+            website:this.state.website,
+            phone_number:this.state.phone_number,
+            hours:this.state.hours
+        }
+        this.setState({
+        modalShow: !this.state.modalShow
 
-    handleSave = () => {
-        const item = this.state;
-        this.props.saveModalDetails(item)
+        })
+}
+
+    modalOpen = () => {
+        this.setState({
+           modalShow : true,
+
+        })
     }
 
-    experienceTypeHandler = (e)=> {
-        this.setState({ experience_type: e.target.value });
-    }
-    experienceSubTypeHandler = (e)=> {
-        this.setState({ experience_sub_type: e.target.value });
-    }
-    experienceDescriptionHandler = (e)=> {
-        this.setState({ experience_description: e.target.value });
-    }
-    addressHandler = (e)=> {
-        this.setState({ address: e.target.value });
-    }
-    websiteHandler = (e) =>{
-        this.setState({ website: e.target.value });
-    }
-    phoneNumberHandler = (e)=> {
-        this.setState({ phone_number: e.target.value });
-    }
-    hoursHandler = (e) =>{
-        this.setState({ hours: e.target.value });
-    }
+    modalClose = () =>
+    this.setState({
+        modalShow: false
+    })
 
-    modalClose = () => this.setState({ modalEditShow: false });
 
     render() {
         return (
             <React.Fragment>
-                <Modal.Dialog>
+                <Modal show = {this.state.modalShow}>
                   <Modal.Header closeButton>
                     <Modal.Title>{this.state.form.experience_name}</Modal.Title>
                   </Modal.Header>
 
                   <Modal.Body>
-                    <p>Experience Type:<input value={this.state.experience_type} onChange={(e) => this.experienceTypeHandler(e)} /></p>
-                    <p>Experience Sub-type:<input value={this.state.experience_sub_type} onChange={(e) => this.experienceSubTypeHandler(e)} /></p>
-                    <p>Description:<input value={this.state.experience_description} onChange={(e) => this.experienceDescriptionHandler(e)} /></p>
-                    <p>Address:<input value={this.state.address} onChange={(e) => this.addressHandler(e)} /></p>
-                    <p>Website:<input value={this.state.website} onChange={(e) => this.websiteHandler(e)} /></p>
-                    <p>Phone Number:<input value={this.state.phone_number} onChange={(e) => this.phoneNumberHandler(e)} /></p>
-                    <p>Hours:<input value={this.state.hours} onChange={(e) => this.hoursHandler(e)} /></p>
+                    <Form>
+                        <label>Experience Type:</label>
+                        <p><input type="text" value={this.state.experience_type} onChange={(e) => this.experienceTypeHandler(e)} /></p>
+                        <p>Experience Sub-type:<input type="text" value={this.state.experience_sub_type} onChange={(e) => this.experienceSubTypeHandler(e)} /></p>
+                        <p>Description:<input type="text" value={this.state.experience_description} onChange={(e) => this.experienceDescriptionHandler(e)} /></p>
+                        <p>Address:<input type="text" value={this.state.address} onChange={(e) => this.addressHandler(e)} /></p>
+                        <p>Website:<input type="text" value={this.state.website} onChange={(e) => this.websiteHandler(e)} /></p>
+                        <p>Phone Number:<input type="text" value={this.state.phone_number} onChange={(e) => this.phoneNumberHandler(e)} /></p>
+                        <p>Hours:<input type="text" value={this.state.hours} onChange={(e) => this.hoursHandler(e)} /></p>
+                    </Form>
                   </Modal.Body>
 
-                  <Modal.Footer>
-                    <Button variant="secondary">Close</Button>
-                    <Button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => { this.handleSave() }}>Save changes</Button>
-                  </Modal.Footer>
-                </Modal.Dialog>;
+                    <Modal.Footer>
+                        <Button onClick={this.modalClose} variant="secondary"> Close </Button>
+                        <Button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.handleSave}> Save Changes </Button>
+                    </Modal.Footer>
+
+                </Modal>
+
             </React.Fragment>
-        );
+        )
     }
-    }
+}
 
 export default Modal;
--- app/javascript/components/pages/ExperienceAdmin.js
+++ app/javascript/components/pages/ExperienceAdmin.js
@@ -14,7 +14,7 @@ class ExperienceAdmin extends React.Component {
     modalOpen = () => {
         this.setState({
            modalShow : true,
-           experience: experience
+
         });
     }
      modalEdit = () => {
@@ -23,8 +23,10 @@ class ExperienceAdmin extends React.Component {
         });
     }
 
-
-    modalClose = () => this.setState({ modalShow: false });
+    modalClose = () =>
+    this.setState({
+        modalShow: false
+    });
 
     delete=(id)=>{
         this.props.delete(id)
@@ -35,33 +37,38 @@ class ExperienceAdmin extends React.Component {
 
         return (
             <React.Fragment>
+                <p>
+                <span>Experience Name: {experience.experience_name}  </span>
+                <span>&nbsp;&nbsp;&nbsp;</span>
+                <span>Experience Type: {experience.experience_type}  </span>
+                <span>&nbsp;&nbsp;&nbsp;</span>
+                <span><button onClick={this.modalOpen}> View Details </button></span>
+                <span>&nbsp;&nbsp;&nbsp;</span>
+                <span><button onClick={this.modalOpen}> Edit Info </button></span>
+                <span>&nbsp;&nbsp;&nbsp;</span>
+                <span><button onClick={() => this.delete(experience.id)}> Delete </button></span>
+                </p>
 
-                <p>Experience Name: {experience.experience_name}</p>
-                <p>Experience Type: {experience.experience_type}</p>
-                <br/>
-                <br/>
-                <p><button onClick={() => this.setState({ modalOpen: true })}>View Details</button></p>
-                <p><button onClick={() => this.setState({ modalEdit:true })}>Edit Info</button></p>
-                <p><button onClick={() => this.delete(experience.id)}>Delete</button></p>
+                { this.state.modalShow ? <div onClick={this.modalClose} className="back-drop"></div> : null }
 
-                <Modal show = {this.state.modalShow}>
-                  <Modal.Header>
-                    <Modal.Title>{experience.experience_name}</Modal.Title>
-                  </Modal.Header>
+                <Modal className="modal" show = {this.state.modalShow} >
+                    <Modal.Header>
+                        <Modal.Title>{experience.experience_name}</Modal.Title>
+                    </Modal.Header>
 
-                  <Modal.Body>
-                    <p>Experience Type:{experience.experience_type}</p>
-                    <p>Experience Sub-type:{experience.experience_sub_type}</p>
-                    <p>Description:{experience.experience_description}</p>
-                    <p>Address:{experience.address}</p>
-                    <p>Website:{experience.website}</p>
-                    <p>Phone Number{experience.phone_number}</p>
-                    <p>Hours{experience.hours}</p>
-                  </Modal.Body>
+                    <Modal.Body>
+                        <p>Experience Type: {experience.experience_type}</p>
+                        <p>Experience Sub-type: {experience.experience_sub_type}</p>
+                        <p>Description: {experience.experience_description}</p>
+                        <p>Address: {experience.address}</p>
+                        <p>Website: {experience.website}</p>
+                        <p>Phone Number: {experience.phone_number}</p>
+                        <p>Hours: {experience.hours}</p>
+                    </Modal.Body>
 
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={this.modalClose}>Close</Button>
-                  </Modal.Footer>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.modalClose}>Close</Button>
+                    </Modal.Footer>
                 </Modal>
             </React.Fragment>
         );
