e7cdb76cf069daf6f611820019e4c2610a222be6
learn <learn@learn10.localdomain>
2019-05-01 16:38:22 -0700

    wedsendofday

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-theArray = ['Adrenaline', 'Foodie', 'Outdoors']
-theArray.each do |type|
-  ['Daytime Activities', 'Nighttime Activities', 'Restaraunt $'].each do |subtype|
-    Experience.where(experience_type: type, experience_sub_type: subtype).first_or_create do |experience|
-      experience.experience_name = [type,subtype].join(' ')
-    end
-  end
-end
+Experience.create(experience_name:"", experience_type:"", experience_sub_type:"", experience_description:"", address:"", website:"", hours:"", lat:"", long:"")
