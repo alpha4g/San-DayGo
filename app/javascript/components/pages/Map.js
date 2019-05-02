1209fdd992cc6874fac01da88e903dbf722ccf47
learn <learn@learn08-7.localdomain>
2019-05-02 16:34:46 -0700

    Google Maps Api Finished

---
+++ app/javascript/components/pages/Map.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
+
+class GoogleMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    }
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+  render() {
+    const style = {
+      width: '40vw',
+      height: '40vh',
+      'marginLeft': '0px',
+      'marginRight': '0px'
+    }
+    return (
+      <Map
+        item
+        xs = { 12 }
+        style = { style }
+        google = { this.props.google }
+        onClick = { this.onMapClick }
+        zoom = { 14 }
+        initialCenter = {{ lat: 39.648209, lng: -75.711185 }}
+      >
+        <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Changing Colors Garage' }
+          position = {{ lat: 39.648209, lng: -75.711185 }}
+          name = { 'Changing Colors Garage' }
+        />
+
+      </Map>
+    );
+  }
+}
+export default GoogleApiWrapper({
+    api: (process.env.AIzaSyCrm6eKO4dUOsVUApNMwMZ124rM_FzNm8s)
+})(GoogleMap)
