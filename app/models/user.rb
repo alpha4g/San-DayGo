88aa8ca8bc1d4676b8c69ef1a444f9f4b9040938
learn <learn@learn10.local>
2019-05-03 13:28:45 -0700

    another try

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
 
-validates :name, presence: true, length: { maximum: 25 }
-validates :password, presence: true, length: { minimum: 6}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
